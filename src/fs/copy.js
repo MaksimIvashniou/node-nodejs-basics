41394612b1c824b21cb4f44cd92ede25f6b054ef
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-07 23:01:17 +0300

    feat: implement fs:copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { cp, readdir } from 'fs/promises';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const [target, copy] = ['files', 'files_copy'];
+
+  try {
+    await readdir(import.meta.dirname).then((list) => {
+      if (!list.includes(target) || list.includes(copy)) throw new Error();
+    });
+
+    await cp(
+      path.resolve(import.meta.dirname, target),
+      path.resolve(import.meta.dirname, copy),
+      { recursive: true }
+    ).catch(() => {
+      throw new Error();
+    });
+  } catch (error) {
+    console.error('FS operation failed');
+  }
 };
 
 await copy();
