0dc3ed74503163f2862a26cd3f5001010f4b4816
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-07 23:08:27 +0300

    feat: implement zip:decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const [sourcePath, targetPath] = [
+    path.resolve(import.meta.dirname, 'files', 'archive.gz'),
+    path.resolve(import.meta.dirname, 'files', 'fileToCompress.txt'),
+  ];
+
+  const readStream = createReadStream(sourcePath);
+  const writeStream = createWriteStream(targetPath);
+  const unzip = createUnzip();
+
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
