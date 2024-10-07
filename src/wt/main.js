5054feef298ea83af57ebd4325d863bf222a8b7c
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-08 01:46:29 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import path from 'path';
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const workerPath = path.resolve(import.meta.dirname, 'worker.js');
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCount = cpus().length;
+  const promises = [];
+
+  for (let i = 0; i < cpuCount; i++) {
+    promises.push(
+      new Promise((resolve, reject) => {
+        const worker = new Worker(workerPath);
+        worker.postMessage(10 + i);
+        worker.on('message', (data) => {
+          resolve({
+            status: 'resolved',
+            data: data,
+          });
+        });
+        worker.on('error', () => {
+          reject({
+            status: 'error',
+            data: null,
+          });
+        });
+      })
+    );
+  }
+
+  Promise.all(promises).then((result) => {
+    console.log(result);
+    process.exit();
+  });
 };
 
 await performCalculations();
