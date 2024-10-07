c51eb8086b5c081e88b0fd7041a2f954c2ced863
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-07 23:02:06 +0300

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = path.resolve(import.meta.dirname, 'files');
+
+  try {
+    await readdir(dirPath)
+      .then((list) => {
+        list.forEach((file) => {
+          console.log(file);
+        });
+      })
+      .catch(() => {
+        throw new Error();
+      });
+  } catch (error) {
+    console.error('FS operation failed');
+  }
 };
 
 await list();
