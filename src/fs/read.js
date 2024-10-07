4986a1e9cf6229286bcd31aed2cd33b480064f37
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-07 23:02:19 +0300

    feat: implement fs:read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const dirPath = path.resolve(import.meta.dirname, 'files');
+
+  try {
+    await readFile(path.resolve(dirPath, 'fileToRead.txt'), {
+      encoding: 'utf8',
+    })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(() => {
+        throw new Error();
+      });
+  } catch (error) {
+    console.error('FS operation failed');
+  }
 };
 
 await read();
