7ae5728a4f43c53bbaed2edce4bf4ac5e6b831de
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-07 23:06:56 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(import.meta.dirname, 'files', 'fileToRead.txt');
+  let data = '';
+  createReadStream(filePath, 'utf-8')
+    .on('data', (chunk) => {
+      data += chunk;
+    })
+    .on('end', () => {
+      stdout.write(data + '\n');
+    });
 };
 
 await read();
