e7a4cce3c5632327c29a474f269bacf116a02d85
Maksim Ivashniou <maksim.ivashniou@gmail.com>
2024-10-07 23:02:35 +0300

    feat: implement fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const dirPath = path.resolve(import.meta.dirname, 'files');
+  const [common, target] = ['wrongFilename.txt', 'properFilename.md'];
+
+  try {
+    await fs.readdir(dirPath).then((list) => {
+      if (!list.includes(common) || list.includes(target)) throw new Error();
+    });
+
+    await fs
+      .rename(path.resolve(dirPath, common), path.resolve(dirPath, target))
+      .catch(() => {
+        throw new Error();
+      });
+  } catch (error) {
+    console.error('FS operation failed');
+  }
 };
 
 await rename();
